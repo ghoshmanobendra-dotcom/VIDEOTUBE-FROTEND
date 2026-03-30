0633142b957ed3e28bffd43b7b9db664539e1be2
Manobendra <ghoshmanobendra@gmail.com>
2026-03-30 12:01:21 +0530

    feat: initialize axios instance with base URL and credentials support

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://videotube-backend-lhdc.onrender.com',
+  baseURL: 'https://videotube-backend-lhdc.onrender.com/api/v1',
   withCredentials: true,
 });
 
